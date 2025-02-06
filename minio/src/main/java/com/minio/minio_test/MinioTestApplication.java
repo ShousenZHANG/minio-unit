b5c31dfcb28cd29e19bc5514b507926a46e001db
Eddy <17368139916@163.com>
2025-02-06 19:47:15 +1100

    1.2.0.0-version

--- minio/src/main/java/com/minio/minio_test/MinioTestApplication.java
+++ minio/src/main/java/com/minio/minio_test/MinioTestApplication.java
@@ -3,6 +3,7 @@ package com.minio.minio_test;
 import com.minio.minio_test.config.MinioClientProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,9 +15,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @date 2022/11/29
  */
 @EnableConfigurationProperties({MinioClientProperties.class})
-@ComponentScan(basePackages = "com.minio.minio_test")
-@EnableAutoConfiguration
 @EnableScheduling
+@SpringBootApplication
 public class MinioTestApplication {
 
     public static void main(String[] args) {
